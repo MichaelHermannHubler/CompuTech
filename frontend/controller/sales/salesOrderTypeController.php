77298fd9efdd66ad4686760c930357c939f0851c
s00zie <s.poeltinger@gmail.com>
2018-06-17 21:29:19 +0200

    Create salesOrderTypeController.php

---
+++ frontend/controller/sales/salesOrderTypeController.php
@@ -0,0 +1,23 @@
+<!--/**
+ * Created by PhpStorm.
+ * User: shu
+ * Date: 16.06.2018
+ * Time: 12:57
+ */-->
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/CompuTechX/persistance/dao/dao_sales/SalesOrderDAO.php';
+?>
+
+<form action="salesOrderTypeController.php" method="post">
+    <button type="submit" name="allOrders">Alle Rechnungen anzeigen</button>
+    <button type="submit" name="openOrders">Offene Rechnungen anzeigen</button>
+</form>
+
+<?php
+if (isset($_POST["allOrders"])) {
+    getAllSalesOrders();
+} elseif (isset($_POST["openOrders"])) {
+    getOpenSalesOrders();
+}
+
+?>
