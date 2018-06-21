c9aa4a73d762426110129881453dee6b314ba61e
s00zie <s.poeltinger@gmail.com>
2018-06-21 10:30:07 +0200

    Update salesOrderTypeController.php

--- frontend/controller/sales/salesOrderTypeController.php
+++ frontend/controller/sales/salesOrderTypeController.php
@@ -1,3 +1,5 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <!--/**
  * Created by PhpStorm.
  * User: shu
@@ -5,7 +7,8 @@
  * Time: 12:57
  */-->
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/CompuTech/persistance/dao/dao_sales/SalesOrderDAO.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/CompuTechX/persistance/dao/dao_sales/SalesOrderDAO.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/CompuTechX/persistance/model/SalesOrder.php';
 ?>
 
 <form action="salesOrderTypeController.php" method="post">
@@ -13,48 +16,65 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/CompuTech/persistance/dao/dao_sales/S
     <button type="submit" name="openOrders">Offene Rechnungen anzeigen</button>
 </form>
 
-<table>
-    <tr><td>Rechnungsnummer</td><td>Kunde</td><td>Adresse</td><td>Rechnungsbetrag</td><td>Rechnungsdatum</td><td>Rechnung bezahlt?</td></tr>
+
     <?php
     if (isset($_POST["allOrders"])) {
+        echo "<table class='table table-hover'>";
+        echo "<tr><td>Rechnungs-ID</td><td>Kunde</td><td>Adresse</td><td>Rechnungsdatum</td><td>Rechnung bezahlt?</td></tr>";
         $recs = new SalesOrderDAO();
         $allRecs = $recs->getAllSalesOrders();
+        //var_dump($allRecs);
         foreach ($allRecs as $rec) {
             echo "<tr>";
-            echo "<td>$rec->id</td>";
-            echo "<td>$rec->firstn</td>";
-            echo "<td>$rec->lastn</td>";
-            echo "<td>$rec->street</td>";
-            echo "<td>$rec->postal</td>";
-            echo "<td>$rec->city</td>";
-            echo "<td>$rec->dateCreated</td>";
-            if ($rec->paid=1) {
-                echo "<td><input type='checkbox' name='paid' value='checked' /></td>";
-            } else {
+            echo "<td>" . $rec->getId() . "</td>";
+            echo "<td>" . $rec->getFirst() . " " . $rec->getLast() . "</td>";
+            echo "<td>" . $rec->getStreet() . ", " . $rec->getPostal() . " " . $rec->getCity() . "</td>";
+            echo "<td>" . $rec->getDateCreated() . "</td>";
+
+            if ($rec->getPaid() <> 1) {
                 echo "<td><input type='checkbox' name='paid' /></td>";
+            } else {
+                echo "<td><input type='checkbox' name='paid' checked /></td>";
             }
             echo "</tr>";
         }
+        ?>
+        </table>
+        <form action="salesOrderTypeController.php" method="post">
+            <button type="submit" name="saveChanges">Änderungen speichern</button>
+        </form>
+        <?php
+        if (isset($_POST['saveChanges'])) {
+
+        }
     } elseif (isset($_POST["openOrders"])) {
+        echo "<table class='table table-hover'>";
+        echo "<tr><td>Rechnungs-ID</td><td>Kunde</td><td>Adresse</td><td>Rechnungsdatum</td><td>Rechnung bezahlt?</td></tr>";
         $recs = new SalesOrderDAO();
         $openRecs = $recs->getOpenSalesOrders();
-        foreach ($openRecs as $rec) {
+        //var_dump($openRecs);
+        foreach ((array) $openRecs as $rec) {
             echo "<tr>";
-            echo "<td>$rec->id</td>";
-            echo "<td>$rec->firstn</td>";
-            echo "<td>$rec->lastn</td>";
-            echo "<td>$rec->street</td>";
-            echo "<td>$rec->postal</td>";
-            echo "<td>$rec->city</td>";
-            echo "<td>$rec->dateCreated</td>";
-            if ($rec->paid=1) {
-                echo "<td><input type='checkbox' name='paid' value='checked' /></td>";
-            } else {
+            echo "<td>" . $rec->getId() . "</td>";
+            echo "<td>" . $rec->getFirst() . " " . $rec->getLast() . "</td>";
+            echo "<td>" . $rec->getStreet() . ", " . $rec->getPostal() . " " . $rec->getCity() . "</td>";
+            echo "<td>" . $rec->getDateCreated() . "</td>";
+
+            if ($rec->getPaid() <> 1) {
                 echo "<td><input type='checkbox' name='paid' /></td>";
+            } else {
+                echo "<td><input type='checkbox' name='paid' checked /></td>";
             }
             echo "</tr>";
-    }
-}
+        }
+        echo "</table>";
+        ?>
+        <form action="salesOrderTypeController.php" method="post">
+            <button type="submit" name="saveChanges">Änderungen speichern</button>
+        </form>
+        <?php
+        if (isset($_POST['saveChanges'])) {
 
-?>
-</table>
+        }
+    }
+    ?>
